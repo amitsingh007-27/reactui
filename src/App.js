784ad799339cb3bf8fd70960d0306f65e3642a8b
AMIT SINGH <77317395+amitsingh007-27@users.noreply.github.com>
2022-07-07 22:29:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import Navbar from "./Components/Navbar";
-import Home from "./Components/pages/Home";
-import About from "./Components/pages/About";
-import Services from "./Components/pages/Services";
-
-
+import Navbar from "./Navbar/Navbar";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from "./Components/Pages/About";
+import Contact from "./Components/Pages/Contact";
+import Home from "./Components/Pages/Home";
+import Menu from "./Components/Pages/Menu";
+import Services from "./Components/Pages/Services";
 
 function App() {
-
   return (
-        <div>
-          <About></About>
-          <Navbar></Navbar>
-          <About></About>
-          <Home></Home>
-          <Services></Services>
-        </div>
- 
+       <Router>
+         <Navbar/>
+         <Routes>
 
+           <Route path="/About"element={<About/>}></Route>
+           <Route path="/Home "element={<Home/>}></Route>
+           <Route path="/Menu"element={<Menu/>}></Route>
+           <Route path="/Services"element={<Services/>}></Route>
+           <Route path="/Contact"element={<Contact/>}></Route>
+
+         </Routes>
+       </Router>
   );
-
-}
-
-
+};
 
 export default App;
